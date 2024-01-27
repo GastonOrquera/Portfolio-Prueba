32529e751ab0594bd42af0cd3381f48e344c94e4
GastonOrquera <gastonorquera29@gmail.com>
2024-01-26 21:23:46 -0300

    Portfolio-Prueba

---
+++ main.js
@@ -0,0 +1,24 @@
+var hamburger = document.querySelector('.hamburger');
+
+var nav = document.querySelector('nav');
+
+var navLinks = document.querySelectorAll('nav a');
+
+hamburger.onclick = function () {
+    nav.classList.toggle('active');
+}
+
+navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('active');
+    });
+});
+
+var tog = document.querySelector('.toggle');
+
+var body = document.querySelector('body');
+
+tog.onclick = function () {
+    tog.classList.toggle('active');
+    body.classList.toggle('active');
+}
