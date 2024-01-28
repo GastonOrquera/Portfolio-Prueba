39405f0748277da722fbcab9d9dbd238f875e2bb
GastonOrquera <gastonorquera29@gmail.com>
2024-01-27 23:59:20 -0300

    porfolio_prueb_

--- main.js
+++ main.js
@@ -21,6 +21,21 @@ var body = document.querySelector('body');
 tog.onclick = function () {
     tog.classList.toggle('active');
     body.classList.toggle('active');
-
 }
+
+ScrollReveal().reveal('.scroll-revealLeft', {
+    origin: 'left',
+    duration: 1000,
+    interval: 200,
+    easing: 'cubic-bezier(0.5,0,0,1)',
+    distance: '40px'
+});
+
+ScrollReveal().reveal('.scroll-revealRight', {
+    origin: 'right',
+    duration: 2000,
+    interval: 400,
+    easing: 'cubic-bezier(0.5,0,0,1)',
+    distance: '20px'
+})
 
