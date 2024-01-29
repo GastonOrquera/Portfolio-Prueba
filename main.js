5c4f1ff078550c09006617a76a8eb78671063403
GastonOrquera <gastonorquera29@gmail.com>
2024-01-29 02:55:09 -0300

    Portfolio-Prueba

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-var hamburger = document.querySelector('.hamburger');
-
-var nav = document.querySelector('nav');
-
-var navLinks = document.querySelectorAll('nav a');
-
-hamburger.onclick = function () {
-    nav.classList.toggle('active');
-};
-
-navLinks.forEach(function (link) {
-    link.addEventListener('click', function () {
-        nav.classList.remove('active');
-    });
-});
-
 var tog = document.querySelector('.toggle');
 
 var body = document.querySelector('body');
@@ -39,7 +23,21 @@ tog.onclick = function () {
     }
 };
 
+var hamburger = document.querySelector('.hamburger');
 
+var nav = document.querySelector('nav');
+
+var navLinks = document.querySelectorAll('nav a');
+
+hamburger.onclick = function () {
+    nav.classList.toggle('active');
+};
+
+navLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+        nav.classList.remove('active');
+    });
+});
 
 ScrollReveal().reveal('.scroll-revealLeft', {
     origin: 'left',
@@ -55,5 +53,5 @@ ScrollReveal().reveal('.scroll-revealRight', {
     interval: 400,
     easing: 'cubic-bezier(0.5,0,0,1)',
     distance: '20px'
-})
+});
 
